7d0c0168bc03895b88a9ba6c9c51eb6afe7a32ef
adieb <mrizqiamiruladieb@gmail.com>
2024-06-03 10:32:07 +0700

    feat: dynamic data - menambahkan item ke array

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <ul>
       <li v-for="item in items" :key="item.id">{{ item.name }}</li>
     </ul>
+    <button @click="addItem">Tambah Item</button>
   </div>
 </template>
 
@@ -16,6 +17,12 @@ export default {
         { id: 2, name: 'Pensil'},
         { id: 3, name: 'Penghapus'}
         ]
+    }
+  },
+  methods: {
+    addItem() {
+      const newItem = { id: this.items.length + 1, name: "Barang Baru"};
+      this.items.push(newItem);
     }
   }
 }
